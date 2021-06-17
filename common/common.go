172b44b3c6d35021cb59a737575bd60085959782
Chris Hager <chris@linuxuser.at>
2021-06-17 11:24:55 +0200

    blockwatch

---
+++ common/common.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	"math/big"
+
+	"github.com/metachris/flashbots/api"
+)
+
+type Bundle struct {
+	Index                 int64
+	Transactions          []api.FlashbotsTransaction
+	TotalMinerReward      *big.Int
+	TotalCoinbaseTransfer *big.Int
+	TotalGasUsed          *big.Int
+
+	CoinbaseDivGasUsed *big.Int
+	RewardDivGasUsed   *big.Int
+}
+
+func NewBundle() *Bundle {
+	return &Bundle{
+		TotalMinerReward:      new(big.Int),
+		TotalCoinbaseTransfer: new(big.Int),
+		TotalGasUsed:          new(big.Int),
+		CoinbaseDivGasUsed:    new(big.Int),
+		RewardDivGasUsed:      new(big.Int),
+	}
+}
